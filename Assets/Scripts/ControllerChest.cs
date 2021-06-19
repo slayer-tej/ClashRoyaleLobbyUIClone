2f2555a8614cf11a2f37884de5e84351497a60c8
slayer-tej <aruntejagurrapu@gmail.com>
2021-06-19 18:13:14 +0530

    Added Chest System, which generates random different types of chest on button click

---
+++ Assets/Scripts/ControllerChest.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ControllerChest : MonoBehaviour
+{
+    public bool unlocked = false;
+    public bool isOpened = false;
+    [SerializeField]
+    private Button _chestButton;
+
+    private void Awake()
+    {
+        _chestButton = GetComponent<Button>();
+        _chestButton.onClick.AddListener(OnClick);
+    }
+
+    private void OnClick()
+    {
+    }
+
+    public void Unlock()
+    {
+        unlocked = true;
+    }
+    public void Open(bool status)
+    {
+        isOpened = status;
+    }
+}
+
